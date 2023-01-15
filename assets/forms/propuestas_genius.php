b69b8231405ec3af24ac95b95e9b4f80b4583518
Alejandro <22056791@live.uem.es>
2023-01-15 19:41:43 +0100

    Parte enviar

--- assets/forms/propuestas_genius.php
+++ assets/forms/propuestas_genius.php
@@ -1,15 +1,29 @@
 <?php
+include "assets/request/conexion.php";
 
-// Recopilar los datos del formulario
-$name = $_POST['name'];
-$message = $_POST['message'];
-$message2 = $_POST['message2'];
+if (isset($_POST['submit'])) {
+    echo "<br>";
 
-// Validar los datos de entrada
-if (empty($name) || empty($surname1) || empty($n_exp) || empty($email) || empty($password)) {
-    // Mostrar mensaje de error
-    exit("Por favor, ingrese todos los datos requeridos.");
-    header("Location: registro.php");
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    $message2 = $_POST['message2'];
+
+    // Prepare the SQL statement
+    $sql = "INSERT INTO proponerproyecto (id, id_usuario, nombre, nexp, email, ideaproyecto, materiales) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "Error: failed to prepare the SQL statement";
+    } else {
+        // Bind the parameters to the placeholders in the SQL statement
+        mysqli_stmt_bind_param($stmt, "iisisss", $id, $id_usuario, $nombre, $n_exp, $email, $password_hash, $role);
+
+        // Execute the SQL statement
+        if (mysqli_stmt_execute($stmt)) {
+            echo "Usuario creado con éxito";
+        } else {
+            echo "Error: failed to execute the SQL statement";
+        }
+        header("Location: /");
+    }
 }
-
 ?>
