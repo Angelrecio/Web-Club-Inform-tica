dc00b002598bab6fdfa9ef873dee717b017d5094
tecno492 <82584661+tecno492@users.noreply.github.com>
2023-01-15 15:50:53 +0100

    registro

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
     $password = mysqli_real_escape_string($conn, $password);
 
     // Consulta para comprobar si el usuario y la contraseña existen en la base de datos
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $query = "SELECT * FROM usuarios WHERE nexp='$username' AND password='$password'";
     $results = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($results) == 1) {
@@ -43,21 +43,21 @@ if (isset($_POST['submit'])) {
 <body style=" flex-direction: column; background-color: rgb(222, 56, 49);">
     <form method="post" action="login.php">
         <div id = "log_pro">
-        <div id = footer_log>
-        <label for="username" class = "login">Nombre de usuario</label>
-        <br>
-        <input style="width: 50%;" type="text" name="username" class = "login" id="username" required>
-        <br>
-        <br>
-        <br>
-        <label for="password" class = "login">Contraseña</label>
-        <br>
-        <input style="width: 50%; " type="password" class = "login" name="password" id="password" required>
-        <br>
-        <br>
-        <br>
-        <input type="submit" class = "login" name="submit" value="Iniciar sesión">
-        </div>
+            <div id = footer_log>
+                <label for="username" class = "login">numero de expediente</label>
+                <br>
+                <input style="width: 50%;" type="text" name="username" class = "login" id="username" required>
+                <br>
+                <br>
+                <br>
+                <label for="password" class = "login">Contraseña</label>
+                <br>
+                <input style="width: 50%; " type="password" class = "login" name="password" id="password" required>
+                <br>
+                <br>
+                <br>
+                <input type="submit" class = "login" name="submit" value="Iniciar sesión">
+            </div>
         </div>
         <span><?php echo $error; ?></span>
     </form>
