d5e15aebbed4ca10a5b0491dbbb5a2f1e6ecd34d
Alejandro <22056791@live.uem.es>
2023-01-17 00:46:12 +0100

    Admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,73 +1,38 @@
-// obtenemos una referencia al elemento video
-var video = document.getElementById("intro_video");
-
-document.getElementById('presentac').style.display = 'block';
-
-    
-// agregamos un evento que se ejecutará cuando el video finalice su reproducción
-video.addEventListener("ended", function() {
-  // aquí eliminamos el elemento video de la página
-  //video.parentNode.removeChild(video);
-  console.log("video borrado");
-    
-  var imagen = document.createElement("img");
-  // y establecer su atributo src para indicar la ruta de la imagen que deseas mostrar
-  imagen.className = "intro";
-  imagen.id = "intro_img"
-  imagen.src = "assets/img/leters.png";
-  
-    
-  console.log(imagen);
-  // luego, puedes reemplazar el elemento video por el elemento img utilizando el método replaceChild():
-  video.parentNode.replaceChild(imagen, video);
-  console.log("video remplazado por imagen");
-
-
-var image = $('#intro_img');
-
-
-setTimeout(() => { 
-  image.animate({
-  width: '12%',
-  top: '10px',
-  marginBottom: "0"
-  }, 500);
- }, 1000);
-
- setTimeout(() => { 
-  
-  logo_uem = document.getElementById("logo_uem");
-  logo_uem.style.display="block";
- }, 1500);
-
-
-
-
-});
 
 $(document).ready(function(){
-  $("#present").click(function(){
-    $("#presentac").slideToggle("slow")
-    $("#propuesta").slideUp("slow")
-    $("#proyect").slideUp("slow")
-    $("#proant").slideUp("slow")
+  $("#talleres1").click(function(){
+    $("#tallerSub").slideToggle("slow")
+    $("#HacknetSub").slideUp("slow")
+    $("#Programacion_CompetitivaSub").slideUp("slow")
+    $("#GeniusXSub").slideUp("slow")
+    $("#UsuariosSub").slideUp("slow")
   });
-  $("#proyecto_ant").click(function(){
-    $("#proant").slideToggle("slow")
-    $("#propuesta").slideUp("slow")
-    $("#proyect").slideUp("slow")
-    $("#presentac").slideUp("slow")
+  $("#Hacknet1").click(function(){
+    $("#HacknetSub").slideToggle("slow")
+    $("#tallerSub").slideUp("slow")
+    $("#Programacion_CompetitivaSub").slideUp("slow")
+    $("#GeniusXSub").slideUp("slow")
+    $("#UsuariosSub").slideUp("slow")
   });
-  $("#proyec_curso").click(function(){
-    $("#proyect").slideToggle("slow")
-    $("#propuesta").slideUp("slow")
-    $("#proant").slideUp("slow")
-    $("#presentac").slideUp("slow")
+  $("#Programacion_Competitiva").click(function(){
+    $("#Programacion_CompetitivaSub").slideToggle("slow")
+    $("#tallerSub").slideUp("slow")
+    $("#HacknetSub").slideUp("slow")
+    $("#GeniusXSub").slideUp("slow")
+    $("#UsuariosSub").slideUp("slow")
   });
-  $("#propues").click(function(){
-    $("#propuesta").slideToggle();
-    $("#proyect").slideUp("slow")
-    $("#proant").slideUp("slow")
-    $("#presentac").slideUp("slow")
+  $("#GeniusX1").click(function(){
+    $("#GeniusXSub").slideToggle();
+    $("#tallerSub").slideUp("slow")
+    $("#HacknetSub").slideUp("slow")
+    $("#Programacion_CompetitivaSub").slideUp("slow")
+    $("#UsuariosSub").slideUp("slow")
+  });
+  $("#Usuarios1").click(function(){
+    $("#UsuariosSub").slideToggle();
+    $("#tallerSub").slideUp("slow")
+    $("#HacknetSub").slideUp("slow")
+    $("#Programacion_CompetitivaSub").slideUp("slow")
+    $("#GeniusXSub").slideUp("slow")
   });
 });
